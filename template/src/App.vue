48702cb870221d77061674a54de8400132c91a7a
Doug Beney <work@dougie.io>
2018-03-04 21:35:31 -0500

    fixed css class

--- template/src/App.vue
+++ template/src/App.vue
@@ -34,7 +34,7 @@ export default {
 </script>
 
 <style lang="sass">
-#app
+#your_app
 	font-family: 'Avenir', Helvetica, Arial, sans-serif
 	-webkit-font-smoothing: antialiased
 	-moz-osx-font-smoothing: grayscale
